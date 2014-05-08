0f518bbcc02ea9a3d4d552edeeb4bc6c3e7b0e3b
James Navin <james.navin@ga.gov.au>
2014-05-08 14:00:23 +1000

    Fixing gitignore to include config folder in src

---
+++ src/main/js/config/directives.js
@@ -0,0 +1,101 @@
+var angular = angular || {};
+var jQuery = jQuery || {};
+var console = console || {};
+
+var app = angular.module('gawebtoolkit.config', []);
+/**
+ * @ngdoc directive
+ * @name gawebtoolkit.config.directives:gaMapConfig
+ * @description
+ *
+ * @restrict E
+ * @example
+ */
+app.directive('gaMapConfig', [ '$compile', '$http', '$q', '$interpolate', '$timeout', '$parse', '$log',
+	function($compile, $http, $q, $interpolate, $timeout, $parse, $log) {
+   'use strict';
+   return {
+      restrict : "E",
+      scope : true,
+      controller : function($scope, $element, $attrs, $http) {
+         $scope.loadConfigData = function() {
+            //init properties
+            var configPath;
+            if ($attrs.gaConfigPath.indexOf('{{') !== -1) {
+               configPath = $scope.$eval($interpolate($attrs.gaConfigPath));
+            } else {
+               configPath = $attrs.gaConfigPath;
+            }
+
+            if ($attrs.staticConfig === 'true') {
+               configPath = configPath + '.json';
+            }
+            var processSuccessResponse = function(data) {
+				$log.info('config http request success');
+               if (data) {
+				   $log.info('config http request data present');
+               }
+               if ($attrs.preConfig) {
+                  var preConfigAssignmentFn = $parse($attrs.preConfig);
+                  $scope.gaConfigTemp = preConfigAssignmentFn($scope, {
+                     config : data
+                  });
+               } else {
+                  $scope.gaConfigTemp = data;
+               }
+
+               //$scope.configReady = true;
+               $scope.$emit('configDataLoaded', $scope.gaConfigTemp);
+               $scope.$broadcast('configDataLoaded', $scope.gaConfigTemp);
+
+               if ($attrs.postConfig) {
+                  var postConfigAssignmentFn = $parse($attrs.postConfig);
+                  postConfigAssignmentFn($scope, {
+                     config : $scope.gaConfigTemp
+                  });
+               }
+               //$scope.$emit('configReady', $scope.configReady);
+            };
+            //try to use provider method first to assign.
+			 $log.info('config loading...');
+            var processErrorResponse = function(data, status, headers, config) {
+				$log.error('Failed to load config - ' + status);
+            };
+            //If it fails, to a get call using the provided path (if exists)
+            if (configPath.length > 0) {
+				$log.info('config http request starting');
+
+               $http({
+                  method : 'GET',
+                  url : configPath
+               }).success(processSuccessResponse).error(processErrorResponse);
+            }
+         };
+      },
+      compile : function compile(element, attributes) {
+         //load templateUrl provided and
+         var linkMethodPromise;
+         var linkMethod;
+         if (attributes.templatePath != null) {
+            var deferred = $q.defer();
+            $http.get(attributes.templatePath).success(function(response) {
+               linkMethod = $compile(response);
+               deferred.resolve(linkMethod);
+            });
+            linkMethodPromise = deferred.promise;
+         }
+
+         return {
+            post : function postLink(scope, element, attributes) {
+
+            },
+            pre : function preLink(scope, element, attributes) {
+               linkMethodPromise.then(function(result) {
+                  element.html(result(scope));
+                  scope.loadConfigData();
+               });
+            }
+         };
+      }
+   };
+} ]);
