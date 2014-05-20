d1469111e7fe92a3a9180ad3f51c69720f3a4d2f
Darren Reid <Darren.Reid@ga.gov.au>
2014-05-20 13:53:40 +1000

    Added another getWMSFeatures function to use wms without a layer and renamed the method that required a layer.

--- src/main/js/map services/data-openlayersv2.js
+++ src/main/js/map services/data-openlayersv2.js
@@ -28,7 +28,61 @@ app.service('WMSDataService', [ '$q', '$http', function ($q, $http) {
             });
             return deferred.promise;
         },
-        getWMSFeatures: function (mapInstance, url, layerId, point) {
+		getWMSFeatures: function (mapInstance, url,layerNames,version,layerProjection, point) {
+			var deferred = $q.defer();
+			var projection = mapInstance.getProjection();
+			var layerProj = layerProjection;
+			if (layerProj && layerProj.equals(mapInstance.getProjectionObject())) {
+				projection = layerProj.getCode();
+			}
+			var params = OpenLayers.Util.extend({
+					service: "WMS",
+					version: version,
+					request: "GetFeatureInfo",
+//					exceptions: firstLayer.params.EXCEPTIONS,
+					bbox: mapInstance.getExtent().toBBOX(null,
+						false),
+					feature_count: 100,
+					height: mapInstance.getSize().h,
+					width: mapInstance.getSize().w,
+					format: OpenLayers.Format.WMSGetFeatureInfo,
+					info_format: 'text/xml'
+				}, (parseFloat(version) >= 1.3) ?
+				{
+					crs: projection,
+					i: parseInt(point.x),
+					j: parseInt(point.y)
+				} :
+				{
+					srs: projection,
+					x: parseInt(point.x),
+					y: parseInt(point.y)
+				}
+			);
+			if (layerNames.length !== 0) {
+				params = OpenLayers.Util.extend({
+					layers: layerNames,
+					query_layers: layerNames
+//					styles: styleNames
+				}, params);
+			}
+			OpenLayers.Util.applyDefaults(params, {});
+			var requestParams = {
+				url: url,
+				params: OpenLayers.Util.upperCaseObject(params),
+				callback: function (request) {
+					var format = new OpenLayers.Format.WMSGetFeatureInfo();
+					var features = format.read(request.responseText);
+					var geoJsonFormat = new OpenLayers.Format.GeoJSON();
+					var geoJsonFeatures = angular.fromJson(geoJsonFormat.write(features));
+					deferred.resolve(geoJsonFeatures);
+				},
+				scope: this
+			};
+			OpenLayers.Request.GET(requestParams);
+			return deferred.promise;
+		},
+        getWMSFeaturesByLayerId: function (mapInstance, url, layerId, point) {
             //This is a modified version of methods used within an OpenLayers control
             //This control was dealing with a data request internally, here we are trying
             //to get extract the data interactions to expose them via this service.
