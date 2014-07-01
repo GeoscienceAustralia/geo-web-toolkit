088ad8006601061465d3edebfe0d362eddc9ae39
Michael de Hoog <michael.dehoog@ga.gov.au>
2014-07-01 11:31:45 +1000

    Added javascript source + minified to gh-pages

--- .travis/push-docs-to-gh-pages.sh
+++ .travis/push-docs-to-gh-pages.sh
@@ -6,7 +6,9 @@ if [ "$TRAVIS_REPO_SLUG" == "GeoscienceAustralia/geo-web-toolkit" ] && [ "$TRAVI
   
   cp -R docs $HOME/docs-latest
   cp -R docs-sources $HOME/docs-sources-latest
-
+  cp -R target/classes/META-INF/resources/webjars/geo-web-toolkit $HOME/dist-latest
+  cp -R target/dist $HOME/dist-latest/latest
+  
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
@@ -15,8 +17,10 @@ if [ "$TRAVIS_REPO_SLUG" == "GeoscienceAustralia/geo-web-toolkit" ] && [ "$TRAVI
   cd gh-pages
   git rm -rf ./docs > /dev/null
   git rm -rf ./docs-sources > /dev/null
+  git rm -rf ./dist/latest > /dev/null
   cp -Rf $HOME/docs-latest ./docs
   cp -Rf $HOME/docs-sources-latest ./docs-sources
+  cp -Rf $HOME/dist-latest ./dist
   git add -f .
   git commit -m "Lastest ng-docs on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null
