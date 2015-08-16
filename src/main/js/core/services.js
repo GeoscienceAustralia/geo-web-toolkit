e6af560833ae5dcb807d3586e42712686fba68ea
Jabir Amirali <jabir.amirali@ga.gov.au>
2015-08-17 09:42:50 +1000

    fix convertHexToRgb scope issues

--- src/main/js/core/services.js
+++ src/main/js/core/services.js
@@ -5,6 +5,10 @@ var $ = $ || {};
 var app = angular.module('gawebtoolkit.utils', []);
 app.service('GAWTUtils', [ function() {
    'use strict';
+   function convertHexToRgb(hexVal) {
+       hexVal = parseInt(hexVal.charAt(0) === '#'? hexVal.substring(1,7): hexVal, 16);
+       return [Math.floor(hexVal / 65536), Math.floor((hexVal % 65536) / 256), hexVal % 256];
+   }
    return {
       generateUuid : function() {
          //Code from - https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
@@ -16,10 +20,7 @@ app.service('GAWTUtils', [ function() {
             return v.toString(16);
          });
       },
-      convertHexToRgb: function (hexVal) {
-          hexVal = parseInt(hexVal.charAt(0) === '#'? hexVal.substring(1,7): hexVal, 16);
-          return [Math.floor(hexVal / 65536), Math.floor((hexVal % 65536) / 256), hexVal % 256];
-      },
+      convertHexToRgb: convertHexToRgb,
       convertHexAndOpacityToRgbArray: function (hexVal, opacity) {
          var a = convertHexToRgb(hexVal);
          a.push(opacity);
