65cc7df1e2db697c9bdc861f2829ed978c7aaee2
Darren Reid <darren.reid@ga.gov.au>
2016-03-09 13:42:08 +1100

    Update karma related deps

--- src/test/js/karma.ci.conf.js
+++ src/test/js/karma.ci.conf.js
@@ -8,9 +8,16 @@ module.exports = function (config) {
     config.set({
         singleRun: true,
         autoWatch: false,
-        reporters: ['progress', 'coverage', 'coveralls'],
+        reporters: ['progress','coverage', 'coveralls'],
         browsers: ['PhantomJS'],
-        plugins: ['karma-jasmine', 'karma-phantomjs-launcher', 'karma-coverage', 'karma-coveralls'],
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-phantomjs-launcher',
+            'karma-ng-html2js-preprocessor',
+            'karma-coverage',
+            'karma-coveralls'
+        ],
         coverageReporter: {
             type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
             dir: 'coverage/'
